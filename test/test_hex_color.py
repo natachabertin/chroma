88a0c72d87b051ba6b11137f76b42b98423612b7
natachabertin <natachabertin@hotmail.com>
2020-08-21 21:53:07 -0300

    Added tests for change color temperature functions.

--- test/test_hex_color.py
+++ test/test_hex_color.py
@@ -19,3 +19,68 @@ class TestHexColor(unittest.TestCase):
     def test__get_hex_name_returns_seven_chars(self):
         result = HexColor('012345')
         self.assertEqual(len(str(result)), 7)
+
+    def test_changeTemp_subtleTrue_shiftsOnesDigit(self):
+        color = HexColor('484848')
+        result = color.warmer(1, True)
+        self.assertEqual(result, '494847')
+
+    def test_changeTemp_subtleFalse_shiftsSixteensDigit(self):
+        color = HexColor('484848')
+        result = color.warmer(1, False)
+        self.assertEqual(result, '584838')
+
+    def test_changeTemp_returnsHexValue(self):
+        color = HexColor('484848')
+        result = color.warmer(3, True)
+        self.assertEqual(result, '4b4845')
+
+    def test_changeTemp_inRangeBoth_shiftFullBR(self):
+        color = HexColor('484848')
+        result = color.warmer(1, False)
+        self.assertEqual(result, '584838')
+
+    def test_changeTemp_higherThanRangeRed_shiftBothSameAmount(self):
+        color = HexColor('484848')
+        result = color.warmer(10, False)
+        self.assertEqual(result, '584838')
+
+    def test_changeTemp_lowerThanRangeRed_shiftBothSameAmount(self):
+        color = HexColor('484848')
+        result = color.cooler(10, False)
+        self.assertEqual(result, '584838')
+
+    def test_changeTemp_higherThanRangeBlue_shiftBothSameAmount(self):
+        color = HexColor('484848')
+        result = color.cooler(10, False)
+        self.assertEqual(result, '584838')
+
+    def test_changeTemp_lowerThanRangeBlue_shiftBothSameAmount(self):
+        color = HexColor('484848')
+        result = color.warmer(10, False)
+        self.assertEqual(result, '584838')
+
+    def test_changeTemp_zeroAmount_doesntShift(self):
+        color = HexColor('484848')
+        result = color.warmer(0, False)
+        self.assertEqual(result, '484848')
+
+    def test_cooler_positiveAmount_callsChangeTempWithInvertedSign(self):
+        color = HexColor('484848')
+        result = color.cooler(10, False)
+        self.assertEqual(result, '584838')
+
+    def test_cooler_negativeAmount_callsChangeTempWithInvertedSign(self):
+        color = HexColor('484848')
+        result = color.cooler(10, False)
+        self.assertEqual(result, '584838')
+
+    def test_warmer_positiveAmount_callsChangeTempWithSameSign(self):
+        color = HexColor('484848')
+        result = color.warmer(10, False)
+        self.assertEqual(result, '584838')
+
+    def test_warmer_negativeAmount_callsChangeTempWithSameSign(self):
+        color = HexColor('484848')
+        result = color.warmer(10, False)
+        self.assertEqual(result, '584838')
